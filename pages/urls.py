68e925706e6a46ba6c3ec8437cf0bbd600d46ac3
Jared Tangir <jaredtangir@Jareds-MacBook-Pro-3.local>
2020-07-14 06:40:44 -0400

    updated titles, add lnv and ggrc ports

--- pages/urls.py
+++ pages/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
 
 
     ## PORTFOLIO DETAILS ##
-    path('portfolio/event-dispatch', TemplateView.as_view(
+    path('portfolio/event-dispatch-employee-scheduling-app', TemplateView.as_view(
         template_name="pages/portfolio/event_dispatch.html"), name="port_event_dispatch"),
+    path('portfolio/ggrc-info-site', TemplateView.as_view(
+        template_name="pages/portfolio/ggrcleaners.html"), name="port_ggrc"),
+    path('portfolio/love-note-video-interactive-web-application', TemplateView.as_view(
+        template_name="pages/portfolio/lovenotevideo.html"), name="port_lovenotevideo"),
 ]
