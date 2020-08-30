93d45e31265a771172a1acf32aa5d125a52c2edc
Jared Tangir <jaredtangir@Jareds-MacBook-Pro-3.local>
2020-08-30 18:47:25 -0400

    create sitemap

---
+++ pages/sitemaps.py
@@ -0,0 +1,10 @@
+from django.contrib.sitemaps import Sitemap
+from django.shortcuts import reverse
+
+
+class StaticViewSitemap(Sitemap):
+    def items(self):
+        return ['home', 'contact_us', 'port_event_dispatch', 'port_ggrc', 'port_lovenotevideo', 'port_mitzvahrsvp', 'port_catertogo', 'port_event_xtras', 'port_exteriordd']
+
+    def location(self, item):
+        return reverse(item)
