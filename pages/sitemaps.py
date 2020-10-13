3953e545c9d4bafc6f05e282d4d18280b2f07722
Jared Tangir <jaredtangir@Jareds-MacBook-Pro-3.local>
2020-10-12 20:21:55 -0400

    add new portfolio objects

--- pages/sitemaps.py
+++ pages/sitemaps.py
@@ -4,7 +4,19 @@ from django.shortcuts import reverse
 
 class StaticViewSitemap(Sitemap):
     def items(self):
-        return ['home', 'contact_us', 'port_event_dispatch', 'port_ggrc', 'port_lovenotevideo', 'port_mitzvahrsvp', 'port_catertogo', 'port_event_xtras', 'port_exteriordd', 'local_doylestown']
+        return ['home',
+                'contact_us',
+                'port_event_dispatch',
+                'port_ggrc',
+                'port_lovenotevideo',
+                'port_mitzvahrsvp',
+                'port_catertogo',
+                'port_event_xtras',
+                'port_exteriordd',
+                'port_sepacoldwar',
+                'port_infinity_caterers',
+                'local_doylestown'
+                ]
 
     def location(self, item):
         return reverse(item)
